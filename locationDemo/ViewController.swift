0d07926f58d4dfa9e9c29c311484f75c9f7aee8b
rizul <rizulgoyal23@gmail.com>
2020-01-09 17:45:53 -0500

    locationnn

--- locationDemo/ViewController.swift
+++ locationDemo/ViewController.swift
@@ -8,14 +8,19 @@
 
 import UIKit
 import CoreLocation
+import MapKit
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
     
+    @IBOutlet var mapView: MKMapView!
     //create the variable of CLLocationManager
     var locationManager = CLLocationManager();
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+     //   mapView.showsUserLocation = true
+        
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestWhenInUseAuthorization()
@@ -29,6 +34,34 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         
         let userLocation: CLLocation = locations[0]
         //print(userLocation)
+        
+        let lat = userLocation.coordinate.latitude
+        let lang = userLocation.coordinate.longitude
+        
+        let latDelta : CLLocationDegrees = 0.05
+            let longDelta : CLLocationDegrees = 0.05
+            
+            //defione span
+            let span = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: longDelta)
+            
+            // define location
+            
+        let location = CLLocationCoordinate2D(latitude: lat, longitude: lang)
+            
+            // define the region
+            
+            let region = MKCoordinateRegion(center: location, span: span)
+            
+            // set the region on the map
+            mapView.setRegion(region, animated: true)
+            
+            //adding annotation for the map
+            
+            let annotation = MKPointAnnotation()
+            annotation.title = "Current Place"
+            annotation.subtitle = "Lets take the tour"
+            annotation.coordinate = location
+            mapView.addAnnotation(annotation)
         
         CLGeocoder().reverseGeocodeLocation(userLocation){(placemarks, error) in
             if let error = error
@@ -68,7 +101,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
                         country = placemark.country!
                     }
                     
-                    print(subthoroughFare + "\n" + thoroughFare`    ` + "\n" + subLocality + "\n" +
+                    print(subthoroughFare + "\n" + thoroughFare + "\n" + subLocality + "\n" +
                     subAdministrativeArea + "\n" + postalCode + "\n" + country)
                 }
             }
